6ecedeae091a0833dbcc5dc94cdbe4ee9cc404bd
labuscmi <michael.labuschagne@vcontractor.co.za>
2020-04-25 23:43:06 +0200

    Created S3 bucket, set bucket to public and static web hosted Add bucket deployment to supply website resources from assets folder Create Cognito UserPool Create UserPool App client with no client secret

--- src/main/java/com/myorg/ServerlessWorkshopWildrydesStack.java
+++ src/main/java/com/myorg/ServerlessWorkshopWildrydesStack.java
@@ -1,8 +1,18 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.cognito.UserPool;
+import software.amazon.awscdk.services.cognito.UserPoolClient;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.deployment.BucketDeployment;
+import software.amazon.awscdk.services.s3.deployment.Source;
+import software.amazon.awscdk.services.ssm.StringParameter;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class ServerlessWorkshopWildrydesStack extends Stack {
     public ServerlessWorkshopWildrydesStack(final Construct scope, final String id) {
@@ -12,6 +22,40 @@ public class ServerlessWorkshopWildrydesStack extends Stack {
     public ServerlessWorkshopWildrydesStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        UserPool userPool = UserPool.Builder.create(this, "WildRydesUserPool")
+                .userPoolName("WildRydes")
+                .build();
+
+        UserPoolClient userPoolClient = UserPoolClient.Builder.create(this,"WildRydesAppClient")
+                .userPool(userPool)
+                .userPoolClientName("WildRydesApp")
+                .generateSecret(false)
+                .build();
+
+        Bucket websiteBucket = Bucket.Builder.create(this, "WildRydesWebsiteBucket")
+                .bucketName("wildrydes-yeehah-"+randomChars())
+                .publicReadAccess(true)
+                .websiteIndexDocument("index.html")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        BucketDeployment websiteContents = BucketDeployment.Builder.create(this, "WildRydesWebsiteDeployment")
+                .sources(Arrays.asList(Source.asset("./assets/website")))
+                .destinationBucket(websiteBucket)
+                .build();
+    }
+
+    private String randomChars() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 8;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
